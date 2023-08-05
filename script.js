6d778104e42701c17c785cc00922eb02f4ddd362
WozLearningJS <sakargyle@gmail.com>
2023-08-05 16:18:28 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 (function() {
-    
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
-    let backspace = document.querySelector('.btn-backspace');
 
-    //menampilkan angka kedalam layar
-    buttons.forEach(button => {
-        button.addEventListener('click', function(e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-        });
+//get elemen
+const screen = document.querySelector('.screen');
+const buttons = document.querySelectorAll('.btn');
+const equal = document.querySelector('.btn-equal');
+const backspace = document.querySelector('.btn-backspace');
+const clear = document.querySelector('.btn-clear');
+
+
+
+//memasukan inputan ke layar
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(e){
+
+       let value = e.target.dataset.num;
+        screen.value += value;
     });
+});
 
-
-    //menjumlahkan
-    equal.addEventListener('click', function(e) {
-        if(screen.value === '') {
-            screen.value = "";
-        } else {
-            let answer = eval(screen.value);
-            screen.value = answer;
-        }
-    });
-
-    //hapus
-    backspace.addEventListener('click', function() {
-        if(screen.value === '') {
-            screen.value = '';
-        } else {
-            let inputAkhir = screen.value;
-            answer = inputAkhir.slice(0, -1);
-            screen.value = answer;
-        }
-    });
-
-
-    //reset
-    clear.addEventListener('click', function() {
+//menjumlahkan
+equal.addEventListener('click', function(){
+    if(screen.value === '') {
         screen.value = '';
-    })
-    
+    } else {
+       let answer = eval(screen.value);
+       screen.value = answer;
+    }
+});
+
+//menghapus
+backspace.addEventListener('click', function() {
+    if(screen.value === '') {
+        screen.value = '';
+    } else {
+        let inputan = screen.value;
+        let hapus = inputan.slice(0,-1);
+        screen.value = hapus;
+    }
+});
+
+//mereset
+clear.addEventListener('click', function() {
+    if(screen.value === '') {
+        screen.value = '';
+    } else {
+         screen.value = '';
+    }
+});
+
 }) ();
